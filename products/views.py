56f8ae9b2d771a86b3d96cb606e29eb33f0b1bfe
unmeshbari <unmesh.bari@rheal.com>
2026-07-07 17:23:07 +0530

    first commit

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
