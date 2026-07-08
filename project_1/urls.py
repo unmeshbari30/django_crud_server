f2e8e5c3f1a4cf7407e58d192bdf5ccd7e5a2f70
unmeshbari <unmesh.bari@rheal.com>
2026-07-08 16:49:11 +0530

    - Added home page route

--- project_1/urls.py
+++ project_1/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+
+def home(request):
+    return HttpResponse("Welcome to Project 1 API")
+
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
 ]
